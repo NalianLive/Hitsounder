490af44c9eccfcea05dcfa5682c50cbc9e0ce4d3
Andrew Austin <andrew@cantharus.me>
2020-08-03 17:29:07 +0100

    Create OutputGenerator skeleton

---
+++ Hitsounder/OutputGenerator.cs
@@ -0,0 +1,39 @@
+﻿using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System;
+using System.Linq;
+
+namespace Hitsounder
+{
+    public interface ISampleManager
+    {
+        ISampleProvider GetSampleProvider(short type, short index);
+    }
+
+    public class OutputGenerator
+    {
+        public const double OutputSampleRate = 44100;
+
+        public ISampleManager SampleManager { get; set; }
+
+        public OutputGenerator(ISampleManager sampleManager)
+        {
+            SampleManager = sampleManager;
+        }
+
+        public ISampleProvider Generate(double songLength, TimingPoint[] timingPoints)
+        {
+            var offsetSampleProviders = new OffsetSampleProvider[timingPoints.Length];
+
+            foreach (var (timingPoint, index) in timingPoints.Select((item, index) => (item, index)))
+            {
+                offsetSampleProviders[index] = new OffsetSampleProvider(SampleManager.GetSampleProvider(timingPoint.SampleType, timingPoint.SampleIndex))
+                {
+                    DelayBy = TimeSpan.FromMilliseconds(timingPoint.Position),
+                };
+            }
+
+            return new MixingSampleProvider(offsetSampleProviders);
+        }
+    }
+}
