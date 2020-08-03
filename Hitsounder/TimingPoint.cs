56e559106549d3cf994857c57375ffd45146cfa4
Andrew Austin <andrew@cantharus.me>
2020-08-03 16:03:28 +0100

    Define TimingPoint

---
+++ Hitsounder/TimingPoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Hitsounder
+{
+    /// <summary>
+    /// A simplified internal representation of an osu! timing point.
+    /// </summary>
+    internal struct TimingPoint
+    {
+        /// <summary>
+        /// The offset from the start of the beatmap in milliseconds.
+        /// </summary>
+        public long Position { get; set; }
+
+        /// <summary>
+        /// The sample type of the hitsound.
+        /// </summary>
+        public short SampleType { get; set; }
+
+        /// <summary>
+        /// The sample index of the hitsound.
+        /// </summary>
+        public short SampleIndex { get; set; }
+    }
+}
