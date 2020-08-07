e06731945e0e13722418e2c911cdc34d9e0bda43
Lewis Crichton <lewistehminerz@outlook.com>
2020-08-07 16:14:04 +0100

    what the fuck

--- Hitsounder/MainWindow.xaml.cs
+++ Hitsounder/MainWindow.xaml.cs
@@ -1,28 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Reflection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace Hitsounder
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnSelectMap_Click(object sender, RoutedEventArgs e)
+        {
+            using (var ofd = new CommonOpenFileDialog())
+            {
+                ofd.Multiselect = false;
+                ofd.EnsurePathExists = true;
+                ofd.ShowDialog();
+            }
+        }
+
+        private void btnSelectSkin_Click(object sender, RoutedEventArgs e)
+        {
+            using (var ofd = new CommonOpenFileDialog())
+            {
+                ofd.IsFolderPicker = true;
+                ofd.Multiselect = false;
+                ofd.EnsurePathExists = true;
+
+                ofd.FileOk += (s, param) =>
+                {
+                    var dialog = s as CommonOpenFileDialog;
+                    var files = new Collection<string>();
+                    typeof(CommonFileDialog)
+                        .GetMethod("PopulateWithFileNames", BindingFlags.Instance | BindingFlags.NonPublic)
+                        ?.Invoke(dialog, new object[] { files });
+
+                    if (File.Exists(Path.Combine(files[0], "skin.ini"))) return;
+                    param.Cancel = true;
+                    MessageBox.Show(
+                        "This does not appear to be a valid skin. Please select a valid skin and try again.", 
+                        "Hitsounder", 
+                        MessageBoxButton.OK, 
+                        MessageBoxImage.Error
+                        );
+                };
+
+                ofd.ShowDialog();
+            }
         }
     }
 }
